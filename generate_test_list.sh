11622f44e85733f52af0b8fa0a00cdebc090c695
lmmv <yves@lmmv.com>
2022-05-24 14:20:33 -0400

    .

--- generate_test_list.sh
+++ generate_test_list.sh
@@ -2,3 +2,6 @@
 rm list.txt -f
 echo "LTRX-111_test_fake" > list.txt
 echo "LTRX-112_test_fake" >> list.txt
+echo "LTRX-11234" >> list.txt
+echo "LTRX-10000" >> list.txt
+echo "LTRX-10011" >> list.txt
